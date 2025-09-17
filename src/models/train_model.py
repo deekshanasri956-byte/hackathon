97a5c30985373270fa26cc4c8dbd353ccd547c40
Avinash <avinashmurukanandhan@gmail.com>
2025-09-17 15:00:58 +0530

    Delete src directory

--- src/models/train_model.py
+++
@@ -1,9 +0,0 @@
-from prophet_model import train_prophet
-from lstm_model import train_lstm
-
-if __name__ == "__main__":
-    # Train Prophet
-    train_prophet()
-
-    # Train LSTM
-    train_lstm()
